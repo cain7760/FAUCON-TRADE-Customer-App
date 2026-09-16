776317f7fa2b0dc0b642dbbbdab4f3edee73c8c7
张启寒 <zhangqihan@MacBook-Pro.local>
2026-09-16 12:23:16 +0800

    Place close position summary above modules

--- src/views/equity/components/ClosePositionDialog.vue
+++ src/views/equity/components/ClosePositionDialog.vue
@@ -18,9 +18,9 @@ function submit(order) { emit('submit', order); close() }
   <el-dialog :model-value="modelValue" top="var(--ft-dialog-top)" width="620px" :teleported="false" :show-close="false" class="close-position-dialog" @update:model-value="emit('update:modelValue', $event)">
     <template #header><header class="close-position-header"><h2>平仓</h2><button type="button" aria-label="关闭平仓" @click="close"><el-icon><Close /></el-icon></button></header></template>
     <div v-if="position && account" class="close-position-layout">
+      <section class="close-position-metrics"><header>持仓概览</header><div><span>持仓股数</span><b>{{ number(position.qty) }} 股</b></div><div><span>持仓市值(万)</span><b>{{ marketValueWan }}</b></div><div><span>可平股数</span><b>{{ number(position.available) }} 股</b></div><div><span>成本价</span><b>{{ money(position.cost) }}</b></div></section>
       <section class="close-market"><OrderBook :symbol="position" :ia-ratio="iaRatio" hide-header hide-ticks @quote="quote = $event" /></section>
       <section class="close-order-side ticket-pane">
-        <div class="close-position-metrics"><div><span>持仓股数</span><b>{{ number(position.qty) }} 股</b></div><div><span>持仓市值(万)</span><b>{{ marketValueWan }}</b></div><div><span>可平股数</span><b>{{ number(position.available) }} 股</b></div><div><span>成本价</span><b>{{ money(position.cost) }}</b></div></div>
         <OrderTicket close-mode :instruments="[]" :accounts="[account]" :symbol="position" :account="account" :quote="quote" :paused="false" @order="submit" />
       </section>
     </div>
